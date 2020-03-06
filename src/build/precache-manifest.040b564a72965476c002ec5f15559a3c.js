d7123e3af7ffd2a97fcf5c7f88534e232ab86b25
Daniel San <eduardo.daniel.s.m@gmail.com>
2020-03-06 17:44:10 -0500

    First commit

---
+++ src/build/precache-manifest.040b564a72965476c002ec5f15559a3c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f86b23f45b6934beb00b372def5ff635",
+    "url": "/index.html"
+  },
+  {
+    "revision": "62800f87600d2bf1b0f3",
+    "url": "/static/css/main.4a9d094a.chunk.css"
+  },
+  {
+    "revision": "7c8116009aec3592ccf0",
+    "url": "/static/js/2.d275a7a6.chunk.js"
+  },
+  {
+    "revision": "f231859d6585c4cd5f80c344783ed269",
+    "url": "/static/js/2.d275a7a6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "62800f87600d2bf1b0f3",
+    "url": "/static/js/main.cd000d40.chunk.js"
+  },
+  {
+    "revision": "9b6997887a9526997e25",
+    "url": "/static/js/runtime-main.908eef38.js"
+  }
+]);
